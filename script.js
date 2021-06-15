59126ab382c0fc6a7674b390c02c963fb7580c40
Jhon Marreros Guzman <j_marreros@hotmail.com>
2021-06-15 09:55:12 -0500

    Corrección reporte de avance

--- script.js
+++ script.js
@@ -23,7 +23,8 @@
             dataType: 'json',
 			success: function(res){
                 if ( res.status  == 0){
-    				$('.process-info').text('Procesados: ' + res.count + ' de : ' + res.total);
+    				$('.process-info').html(`<strong>Procesados ${res.count} de ${res.total}
+                                            <br> Paso: ${res.step}</strong>`);
                     dcms_process_step(res.step, res.total)
                 } else {
                     $('.process-info').text('Finalizado');
